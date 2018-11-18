36598a78ea15f4256f56f11ae1752bbc039f60ab
Geoff MacDonald <geoffmacd@gmail.com>
2018-11-17 22:19:02 -0800

    Initial Commit

---
+++ CleanNewEvent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanNewEvent
+//
+//  Created by Geoff MacDonald on 11/17/18.
+//  Copyright © 2018 Geoff MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
